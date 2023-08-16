c0b9d2e60b8dab18947ec3b3fcac7cb2128dc189
alef-sena <alefsena@senseup.tech>
2023-08-16 16:52:59 -0300

    ✨ implement initial dashboard and team page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import { useState } from 'react';
+import Teams from './scenes/team';
 // import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
 // import Contacts from './scenes/contacts';
@@ -30,16 +31,16 @@ function App() {
 						<Topbar setIsSidebar={setIsSidebar}/>
 						<Routes>
 							<Route path='/' element={<Dashboard />} />
-							{/* <Route path='/team' element={<Team />} />
-							<Route path='/contacts' element={<Contacts />} />
-							<Route path='/invoices' element={<Invoices />} />
-							<Route path='/form' element={<Form />} />
-							<Route path='/bar' element={<Bar />} />
-							<Route path='/pie' element={<Pie />} />
-							<Route path='/line' element={<Line />} />
-							<Route path='/faq' element={<FAQ />} />
-							<Route path='/geography' element={<Geography />} />
-							<Route path='/calendar' element={<Calendar />} /> */}
+							<Route path='/team' element={<Teams />} />
+							{/* <Route path='/contacts' element={<Contacts />} /> */}
+							{/* <Route path='/invoices' element={<Invoices />} /> */}
+							{/* <Route path='/form' element={<Form />} /> */}
+							{/* <Route path='/bar' element={<Bar />} /> */}
+							{/* <Route path='/pie' element={<Pie />} /> */}
+							{/* <Route path='/line' element={<Line />} /> */}
+							{/* <Route path='/faq' element={<FAQ />} /> */}
+							{/* <Route path='/geography' element={<Geography />} /> */}
+							{/* <Route path='/calendar' element={<Calendar />} /> */}
 						</Routes>
 					</main>
 				</div>
