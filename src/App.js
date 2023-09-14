a0403cf088f2e012946a57c9621a7a9b6370c79e
alef-sena <alefsena@senseup.tech>
2023-09-14 11:45:29 -0300

    ✨ implement initial calendar page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar';
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
-// import Calendar from './scenes/calendar';
 
 function App() {
 	const [ theme, colorMode ] = useMode();
@@ -24,22 +24,22 @@ function App() {
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<div className="app">
+				<div className='app'>
 					<Sidebar isSidebar={isSidebar} />
 					<main className='content'>
-						<Topbar setIsSidebar={setIsSidebar}/>
+						<Topbar setIsSidebar={setIsSidebar} />
 						<Routes>
 							<Route path='/' element={<Dashboard />} />
 							<Route path='/team' element={<Team />} />
 							<Route path='/contacts' element={<Contacts />} />
 							<Route path='/invoices' element={<Invoices />} />
 							<Route path='/form' element={<Form />} />
+							<Route path='/calendar' element={<Calendar />} />
 							{/* <Route path='/bar' element={<Bar />} /> */}
 							{/* <Route path='/pie' element={<Pie />} /> */}
 							{/* <Route path='/line' element={<Line />} /> */}
 							{/* <Route path='/faq' element={<FAQ />} /> */}
 							{/* <Route path='/geography' element={<Geography />} /> */}
-							{/* <Route path='/calendar' element={<Calendar />} /> */}
 						</Routes>
 					</main>
 				</div>
